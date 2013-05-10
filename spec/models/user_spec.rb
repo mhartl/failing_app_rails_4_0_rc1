646aebc37b863ba05bca8abb153c9400b6c2fb5a
Michael Hartl <michael@michaelhartl.com>
2013-05-10 12:52:28 -0700

    Further simplify the app

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  let(:user) { User.new(name: "Example User", email: "user@example.com") }
+  let(:user) { User.new(email: "user@example.com") }
   it "should save the user" do
     expect { user.save! }.not_to raise_error
   end
